ee1064bf7d150c4d56031d0a4feebc81ebec1f48
David-TC13 <115069379+David-TC13@users.noreply.github.com>
2023-02-13 15:58:55 +0100

    done

--- src/cleaning_map_df.py
+++ src/cleaning_map_df.py
@@ -23,3 +23,81 @@ def data_together( df_coordinates, df_zg):
     df_all.reset_index(inplace= True)
     df_all.drop(columns=['index'],inplace=True)
     return df_all
+
+
+def points(df_all):
+    sf_lat = 37.7576171
+    sf_lon = -122.486017213
+    sf_map = Map(location=[sf_lat, sf_lon], zoom_start = 11)
+    for index, row in df_all.iterrows():
+        
+        ubication = {"location": [row["lat"], row["lon"]]}
+        if row["name"] == 'Zynga':        
+            icon = Icon (
+                color="white",
+                opacity = 0.6,
+                prefix = "fa",
+                icon="briefcase",
+                icon_color = "black"
+            )
+        elif row["name"] == 'Starbucks':        
+            icon = Icon (
+                color="green",
+                opacity = 0.6,
+                prefix = "fa",
+                icon="coffee",
+                icon_color = "white"
+            )
+        elif row["name"] == 'De Haro & Mariposa':        
+            icon = Icon (
+                color="red",
+                opacity = 0.6,
+                prefix = "fa",
+                icon="bus",
+                icon_color = "black"
+            )
+        elif row["name"] == "San Francisco International Airport (SFO)":        
+            icon = Icon (
+                color="lightblue",
+                opacity = 0.6,
+                prefix = "fa",
+                icon="plane",
+                icon_color = "black"
+            )
+        elif row["name"] == "City College of San Francisco Mission Campus":        
+            icon = Icon (
+                color="gray",
+                opacity = 0.6,
+                prefix = "fa",
+                icon="book",
+                icon_color = "white"
+            )
+        elif row["name"] == "Tower Valet Parking":        
+            icon = Icon (
+                color="white",
+                opacity = 0.6,
+                prefix = "fa",
+                icon="map-marker",
+                icon_color = "blue"
+            )
+        elif row["name"] == "Next Level Burger Potrero Hill":        
+            icon = Icon (
+                color="beige",
+                opacity = 0.6,
+                prefix = "fa",
+                icon="cutlery",
+                icon_color = 'purple'
+            )
+        elif row["name"] == "Berry Basketball Courts":
+            icon = Icon (
+                color="blue",
+                opacity = 0.6,
+                prefix = "fa",
+                icon="futbol-o",
+                icon_color = "brown",
+                icon_size=(14, 14)
+            )
+        new_marker = Marker(**ubication,icon=icon)
+        
+        new_marker.add_to(sf_map)
+    return sf_map
